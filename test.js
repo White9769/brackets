3c928f36700345ea94669673d03fb393062b71d8
White9769 <47785867+White9769@users.noreply.github.com>
2020-10-26 00:39:05 +0300

    Fix test.js
    Fix test.js

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-let stack = [], open = "([{", close = ")]}";
-for (const ch of str)
-{
-let i = open.indexOf(ch);
-if (i > -1) stack.push(close[i]);
-if (close.includes(ch) && ch != stack.pop()) return false
-}
-return stack.length == 0;
-}
+            let stack = [], open = "([{", close = ")]}";
+            for (const ch of str)
+            {
+                let i = open.indexOf(ch);
+                if (i > -1) stack.push(close[i]);
+                if (close.includes(ch) && ch != stack.pop()) return false
+            }
+            return stack.length == 0;
+        }
