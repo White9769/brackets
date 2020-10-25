07ada88c585d13edad954f64023991371ce490a8
White9769 <47785867+White9769@users.noreply.github.com>
2020-10-26 00:57:34 +0300

    Fix index.js
    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+            let stack = [], open = "([{", close = ")]}";
+            for (const ch of str)
+            {
+                let i = open.indexOf(ch);
+                if (i > -1) stack.push(close[i]);
+                if (close.includes(ch) && ch != stack.pop()) return false
+            }
+            return stack.length == 0;
+        }
